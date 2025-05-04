9d3a6ae2dde40100256a4d2171b970dedd68f9eb
israel <zdesenhos@gmail.com>
2025-05-04 20:28:58 -0300

    att responses
    att responses

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const chatForm = document.getElementById('chat-form')
+const chatForm = document.getElementById('chatForm')
 const chatMessages = document.getElementById('chat-box')
 const chatInput = document.getElementById('chat-input')
 
 // Função para imprimir mensagens no DOM
-function appendMessage(sender, text, className) {
+function addMessage(sender, text, className) {
   const msg = document.createElement('div')
   msg.className = `message ${className}`
   // msg.innerText = text
@@ -18,7 +18,7 @@ chatForm.addEventListener('submit', async (e) => {
   const userMsg = chatInput.value.trim()
   if (!userMsg) return
   // Adiciona a mensagem do usuário ao chat visualmente
-  appendMessage("Você", userMsg, 'user-message')
+  addMessage("Você", userMsg, 'user-message')
   // Limpa o input
   chatInput.value = ''
 
@@ -32,8 +32,8 @@ chatForm.addEventListener('submit', async (e) => {
     
     const data = await response.json()
     // Exibe a resposta do bot no chat
-    appendMessage("FURIA BOT", data.response, 'bot-message')
+    addMessage("FURIA", data.response, 'bot-message')
   } catch (err) {
-    appendMessage('Erro ao se comunicar com o bot.', 'bot-message')
+    addMessage('Erro ao se comunicar com o servidor.', 'bot-message')
   }
 })
